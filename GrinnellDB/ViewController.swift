0fb77ef9b7694f65ed60b6b66e06e755d7e409d1
Zixuan Guo <i@zxguo.me>
2019-09-07 15:30:22 -0500

    Initial Commit

---
+++ GrinnellDB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrinnellDB
+//
+//  Created by Zixuan on 9/7/19.
+//  Copyright © 2019 Zixuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
