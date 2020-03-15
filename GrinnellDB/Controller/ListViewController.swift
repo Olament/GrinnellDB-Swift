159ff9ede8f0d1a6969c2468a8d5080a4fd59c0d
Zixuan Guo <i@zxguo.me>
2020-03-14 21:54:25 -0500

    add fetch from api

--- GrinnellDB/Controller/ListViewController.swift
+++ GrinnellDB/Controller/ListViewController.swift
@@ -12,6 +12,67 @@ class ListViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        fetch()
+    }
+    
+    // MARK: - Query
+    
+    var params: [String: String?] = [:]
+    let defaults = UserDefaults.standard
+    
+    func fetch() {
+        guard let cookie = defaults.string(forKey: "cookie") else { return }
+        
+        /* translate parameters to query dictionary */
+        let query: [String: String] = ["firstName": (params["First name"] ?? "") ?? "" ,
+                                       "lastName" : (params["Last name"] ?? "") ?? "",
+                                       "email": (params["Computer Username"] ?? "") ?? "",
+                                       "campusPhone": (params["Campus Phone"] ?? "") ?? "",
+                                       "homeAddress": (params["Home Address"] ?? "") ?? "",
+                                       "facultyDepartment": (params["Fac/Staff Dept/Office"] ?? "") ?? "",
+                                       "major": (params["Student Major"] ?? "") ?? "",
+                                       "concentration": (params["Concentration"] ?? "") ?? "",
+                                       "sga": (params["SGA"] ?? "") ?? "",
+                                       "hiatus": (params["Hiatus"] ?? "") ?? "",
+                                       "studentClass": (params["Student Class"] ?? "") ?? "",
+                                       "campusquery": (params["Campus Address or P.O. Box"] ?? "") ?? "",
+                                       "token": cookie]
+       
+        var searchURLComponents = URLComponents(string: "http://appdev.grinnell.edu:3000/api/v1/ios/fetch?")
+                
+        var querys: [URLQueryItem] = []
+        for (key, value) in query {
+            querys.append(URLQueryItem(name: key, value: value))
+        }
+
+        searchURLComponents?.queryItems = querys
+        let url = searchURLComponents!.url!
+        
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+            guard let data = data else { return }
+            
+            print(data.prettyPrintedJSONString)
+            
+            do {
+                let results = try JSONDecoder().decode(QueryResult.self, from: data)
+                                
+                for person in results.content! {
+                    if let student = person as? Student {
+                        self.people.append(student)
+                    } else if let faculty = person as? Faculty {
+                        self.people.append(faculty)
+                    } else if let sga = person as? SGA {
+                        self.people.append(sga)
+                    }
+                }
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            } catch let jsonerr {
+                print(jsonerr)
+            }
+        }.resume()
     }
 
     // MARK: - Table view data source
@@ -39,16 +100,16 @@ class ListViewController: UITableViewController {
             switch person.type {
             case .student:
                 let student = person as! Student
-                detailText = String(format: "%@\n%@\n%@", student.email, student.major, student.classYear)
+                detailText = "\(student.email ?? "Null")\n\(student.major ?? "Null")\n\(student.classYear ?? "Null")\n"
             case .SGA:
                 let sga = person as! SGA
-                detailText = String(format: "%@\n%@\n", sga.email, sga.positionName)
+                detailText = "\(sga.email ?? "Null")\n\(sga.positionName ?? "Null")\n"
             case .faculty:
                 let faculty = person as! Faculty
-                detailText = String(format: "%@\n%@\n%@", faculty.email, faculty.departments[0], faculty.titles[0])
+                detailText = "\(faculty.email ?? "Null")\n\(faculty.department ?? "Null")\n\(faculty.title ?? "Null")\n"
             }
-            
-            cell.name.text = person.firstName + " " + person.lastName
+                        
+            cell.name.text = (person.firstName ?? "") + " " + (person.lastName ?? "")
             
             let paragraphStyle = NSMutableParagraphStyle()
             paragraphStyle.lineSpacing = 2.0
@@ -68,7 +129,7 @@ class ListViewController: UITableViewController {
                 cell.profileImage?.image = image
             } else {
                 DispatchQueue.global(qos: .userInitiated).async { [weak self] in
-                    let urlContents = try? Data(contentsOf: person.imgPath)
+                    let urlContents = try? Data(contentsOf: URL(string: person.imgPath ?? "https://itwebapps.grinnell.edu/PcardImages/moved/link/gone.jpg")!)
                     DispatchQueue.main.async {
                         if let imageData = urlContents {
                             cell.profileImage?.image = UIImage(data: imageData)
@@ -94,5 +155,15 @@ class ListViewController: UITableViewController {
                 detailVC.profileImage = imageCache[selectedIndexPath.row] ?? UIImage(named: "placeholder")
             }
         }
+    }
+}
+
+extension Data {
+    var prettyPrintedJSONString: NSString? { /// NSString gives us a nice sanitized debugDescription
+        guard let object = try? JSONSerialization.jsonObject(with: self, options: []),
+              let data = try? JSONSerialization.data(withJSONObject: object, options: [.prettyPrinted]),
+              let prettyPrintedString = NSString(data: data, encoding: String.Encoding.utf8.rawValue) else { return nil }
+
+        return prettyPrintedString
     }
 }
